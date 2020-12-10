2c2cbe680358d1a7f9e913c14dafd32573571a1d
Daniel Reis <daniel.mvr@gmail.com>
2020-12-10 15:56:45 -0300

    ajust analyctis

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import Main from '../src/Main';
-import ReactGA from 'react-ga';
-
-function initializeReactGA() {
-  ReactGA.initialize('G-GE686WPV6X');
-  ReactGA.pageview('/homepage');
-  console.log('passei no google')
-}initializeReactGA()
 
 export default function App() {
   
