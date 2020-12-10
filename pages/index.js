ec23caea69983718ce635b2e1bff55041833474b
Daniel Reis <daniel.mvr@gmail.com>
2020-12-10 15:39:08 -0300

    ..analytics

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ import ReactGA from 'react-ga';
 function initializeReactGA() {
   ReactGA.initialize('G-GE686WPV6X');
   ReactGA.pageview('/homepage');
-}
+  console.log('passei no google')
+}initializeReactGA()
 
 export default function App() {
+  
   return <Main />
 }
