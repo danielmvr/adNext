c8f32f664291489e91bd32c72f6ce5d7c8e8f25d
Daniel Reis <daniel.mvr@gmail.com>
2020-12-12 13:52:50 -0300

    react-ga

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Main from '../src/Main';
+import ReactGa from 'react-ga';
 
 export default function App() {
-  
+
+  useEffect(() => {
+    ReactGa.initialize('G-GE686WPV6X')
+
+    //to report page view
+    ReactGa.pageview('/')
+  })
+
   return <Main />
 }
